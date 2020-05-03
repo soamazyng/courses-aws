9784f0be41fb87d083b22746263cb469a35c62b2
Jaqueline Benedicto <soamazing@gmail.com>
2020-05-02 21:39:08 -0300

    python rds off and ec2

--- udemy-aws-lambda-and-serverless/python-ec2-start-stop/handler.py
+++ udemy-aws-lambda-and-serverless/python-ec2-start-stop/handler.py
@@ -1,25 +1,40 @@
+import botocore
 import boto3
 
 ec2 = boto3.client('ec2')
+rds = boto3.client('rds')
 
+instances = ['i-09bfb5917e6433936', 'i-0b440ab36a0e4bf74']
 
-def start_ec2(event, context):
-    ec2_instances = get_all_ec2_ids()
-    response = ec2.start_instances(
-        InstanceIds=ec2_instances,
-        DryRun=False
-    )
-    return response
+rds = 'jay'
+
+def start_ec2(event, context):        
+    ec2.start_instances(InstanceIds=instances)
+    print('started your instances: ' + str(instances))
 
 
 def stop_ec2(event, context):
-    ec2_instances = get_all_ec2_ids()
-    response = ec2.stop_instances(
-        InstanceIds=ec2_instances,
-        DryRun=False
-    )
+    ec2.stop_instances(InstanceIds=instances)
+    print('stopped your instances: ' + str(instances))
+
+def start_rds(event, context):
+    result = rds.start_db_instance(DBInstanceIdentifier=rds)
+    response = {
+        "statusCode": 200,
+        "body": json.dumps(result)
+    }
+
     return response
 
+
+def stop_rds(event, context):
+    result = rds.stop_db_instance(DBInstanceIdentifier=rds)
+    response = {
+        "statusCode": 200,
+        "body": json.dumps(result)
+    }
+
+    return response
 
 # get the list of all the ec2 instances
 def get_all_ec2_ids():
